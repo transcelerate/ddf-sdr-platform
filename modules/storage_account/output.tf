e8d7b3ae63105498306cb5e64891feecdc62b85d
SushmitabSinha <98328753+SushmitabSinha@users.noreply.github.com>
2022-02-16 12:49:04 +0530

    Delete modules/storage_account directory

--- modules/storage_account/output.tf
+++
@@ -1,9 +0,0 @@
-output "storage_account_endpoint" {
-  value = azurerm_storage_account.storage_account.primary_blob_endpoint
-}
-output "storage_account_name" {
-  value = azurerm_storage_account.storage_account.name
-}
-output "storage_account_id" {
-  value = azurerm_storage_account.storage_account.id
-}
