8a998d308596b9041d488babe1b8a1a60d97318f
SushmitabSinha <98328753+SushmitabSinha@users.noreply.github.com>
2022-06-13 12:15:18 +0530

    Update appreg.tf

--- modules/azuread_appregistration/appreg.tf
+++ modules/azuread_appregistration/appreg.tf
@@ -2,7 +2,12 @@ data "azuread_client_config" "current" {}
 
 resource "random_uuid" "appreg_scope_id" {}
 
+resource "random_uuid" "appuser_role_id" {}
+
+resource "random_uuid" "orgadmin_role_id" {}
+
 resource "azuread_application" "spn_ui_test" {
+  
   display_name     = var.display_name
   owners           = [data.azuread_client_config.current.object_id]
   identifier_uris  = var.identifier_uris
@@ -20,6 +25,24 @@ resource "azuread_application" "spn_ui_test" {
       type                       = "User"
       value                      = var.oauthvalue
     }
+  }
+
+  app_role {
+    allowed_member_types = ["User"]
+    description          = "Application User Access"
+    display_name         = "App User Role"
+    enabled              = true
+    id                   = random_uuid.appuser_role_id.result
+    value                = "app.user"
+  }
+
+  app_role {
+    allowed_member_types = ["User"]
+    description          = "Organization Admin Access"
+    display_name         = "Org Admin Role"
+    enabled              = true
+    id                   = random_uuid.orgadmin_role_id.result
+    value                = "org.admin"
   }
 
   optional_claims {
@@ -45,6 +68,16 @@ resource "azuread_application" "spn_ui_test" {
     implicit_grant {
       access_token_issuance_enabled = true
       id_token_issuance_enabled     = true
+    }
+  }
+
+  required_resource_access {
+
+    resource_app_id = "00000003-0000-0000-c000-000000000000"  # MS Graph app id.
+
+    resource_access {
+      id   = "df021288-bdef-4463-88db-98f22de89214" # User.Read.All id.
+      type = "Role"
     }
   }
 }
