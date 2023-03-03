5b9452b23e1694a464f715198a59c9df1f49177c
velpula.sravan <velpula.sravan@accenture.com>
2023-03-03 14:21:59 +0530

    updated fun app diag script

--- modules/function_app/output.tf
+++ modules/function_app/output.tf
@@ -3,12 +3,12 @@ output "functionapp_id" {
   value = azurerm_windows_functionapp.functionapp.id
 
 }
-output "functionapp_identity" {
+# output "functionapp_identity" {
 
-  value = azurerm_windows_functionapp.functionapp.identity[0].principal_id
+#   value = azurerm_windows_functionapp.functionapp.identity[0].principal_id
 
-}
-output "functionapp_name" {
+# }
+# output "functionapp_name" {
 
-  value = azurerm_windows_functionapp.functionapp.name
-}
+#   value = azurerm_windows_functionapp.functionapp.name
+# }
